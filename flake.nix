075937b9a76d3573889289a50f3eb852da258f32
Ocramrenegew <wegener-marco@gmx.de>
2026-05-15 02:00:24 +0200

    Removed home file

--- flake.nix
+++ flake.nix
@@ -14,8 +14,6 @@
 
         homeManagerModules.default = { config, ... }: {
             home.packages = [ pkgs.neovim ];
-            home.file.".config/nvim".source =
-                config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nvim-config";
         };
     };
 }
